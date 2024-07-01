2ced86ac8c2c4edb7e207ca5a10d00eac1b70e44
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-01 13:09:05 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Users\PermissionsController;
+use App\Http\Controllers\Users\RolesController;
+
+
+Route::prefix('admin')->group(function () {
+    Route::resource('permissions', PermissionsController::class);
+    Route::post('/permissions/add-role-permision',[PermissionsController::class,'role_permission']);
+    Route::get('/permissions/roles/{id}',[PermissionsController::class,'get_permissions']);
+
+    //===========================================================
+    Route::resource('roles', RolesController::class);
+
+    //===========================================================
+});
