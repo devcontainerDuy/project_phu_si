7e5ca77ed5606d5dd5dd13ebabb7f4289cdb70dd
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-04 17:05:56 +0700

    Commit

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -10,6 +10,8 @@ use Inertia\Inertia;
 use App\Models\Categories\Categories;
 use App\Models\Brands\Brands;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Products\Gallery;
 
 class ProductsController extends Controller
 {
@@ -39,7 +41,40 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'sku' => 'required',
+            'price' => 'required',
+            'compare_price' => 'required',
+            'attributes' => 'required',
+            'discount' => 'required',
+            'description'=>'required',
+            'content'=>'required',
+            'id_brand'=>'required|exists:brands,id',
+            'instock'=>'required|numeric',
+            'id_category'=>'required|exists:categories,id',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['check' => false, 'msg' => $validator->errors()->first()]);
+        }
+        $data['name']=$request->name;
+        $data['sku']=$request->sku;
+        $data['price']=$request->price;
+        $data['compare_price']=$request->compare_price;
+        $data['attributes']=$request->attributes;
+        $data['discount']=$request->discount;
+        $data['description']=$request->description;
+        $data['content']=$request->content;
+        $data['id_brand']=$request->id_brand;
+        $data['in_stock']=$request->instock;
+        $data['id_category']=$request->id_category;
+        $data['created_at']=now();
+        $id=Products::insertGetId($data);
+        $images=$request->images;
+        foreach ($images as $key => $value) {
+            Gallery::create(['model'=>'PRODUCT','image'=>$value,'id_parent'=>$value,'status'=>0,'created_at'=>now()]);
+        }
+        return response()->json(['check'=>true]);
     }
 
     /**
