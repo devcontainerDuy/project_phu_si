cb38946ae80cbc6386472719a89d4a50d2e99eb0
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-04 08:07:28 +0700

    Commirt

---
+++ database/migrations/2024_07_04_080512_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('summary');
+            $table->longText('content');
+            $table->boolean('status')->default(0);
+            $table->boolean('highlighted')->default(0);
+            $table->unsignedInteger('position')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
