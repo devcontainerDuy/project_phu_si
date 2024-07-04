81add41e4cc7014c6f80918e261b2585ad2a1290
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-04 11:40:08 +0700

    Commit

--- app/Models/Products/Products.php
+++ app/Models/Products/Products.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     use HasFactory;
+    protected $table='products';
+    protected $fillable=['id','name','slug','sku','price','compare_price','discount','description','content','id_brand','in_stock','created_at','updated_at'];
 }
