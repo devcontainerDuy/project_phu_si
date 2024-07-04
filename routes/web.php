eaa689c0e24606098578ba526b09e5d9ee607cee
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2024-07-04 16:04:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\Users\RolesController;
 use App\Http\Controllers\Users\UsersController;
 use App\Http\Controllers\Brands\BrandController;
 use App\Http\Controllers\Files\FolderController;
-use App\Http\Controllers\Files\FileController;
 use App\Http\Controllers\Products\ProductsController;
 use App\Http\Controllers\Slide\SlidesController;
 use App\Http\Controllers\Users\PermissionsController;
