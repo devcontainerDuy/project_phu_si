65c4789d0fea99073760dfbc309f6d4c9808437f
Khanh Duy Dev <khanhduytran1803@gmail.com>
2024-07-09 10:47:06 +0700

    new commit

--- app/Http/Controllers/Posts/PostCollectionsController.php
+++ app/Http/Controllers/Posts/PostCollectionsController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Posts;
 
-use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Post\PostCollections;
+use Illuminate\Support\Facades\Validator;
 
 class PostCollectionsController extends Controller
 {
@@ -12,7 +16,8 @@ class PostCollectionsController extends Controller
      */
     public function index()
     {
-        //
+        $data = PostCollections::all();
+        return Inertia::render('Post/PostCollections', ['collections' => $data]);
     }
 
     /**
@@ -28,7 +33,27 @@ class PostCollectionsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:post_collections,name',
+            'position' => 'required|numeric|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['check' => false, 'msg' => $validator->errors()->first()]);
+        }
+
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['name']);
+        $data['created_at'] = now();
+        $data['updated_at'] = now();
+        $created = PostCollections::create($data);
+
+        if ($created) {
+            $collections = PostCollections::all();
+            return response()->json(['check' => true, 'msg' => 'Tạo thành công', 'data' => $collections]);
+        } else {
+            return response()->json(['check' => false, 'msg' => 'Tạo thấtt bại']);
+        }
     }
 
     /**
@@ -52,7 +77,26 @@ class PostCollectionsController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'unique:post_collections,name',
+            'position' => 'numeric|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['check' => false, 'msg' => $validator->errors()->first()]);
+        }
+
+        $data = $request->all();
+        ($request->has('name')) ? $data['slug'] = Str::slug($request->name) : '';
+        $data['updated_at'] = now();
+        $updated = PostCollections::find($id)->update($data);
+
+        if ($updated) {
+            $collections = PostCollections::all();
+            return response()->json(['check' => true, 'msg' => 'Cập nhật thành công', 'data' => $collections]);
+        } else {
+            return response()->json(['check' => false, 'msg' => 'Cập nhật thất bại']);
+        }
     }
 
     /**
@@ -60,6 +104,17 @@ class PostCollectionsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $collections = PostCollections::find($id);
+        if (!$collections) {
+            return response()->json(['check' => false, 'msg' => 'Không tìm thấy collection']);
+        }
+        $deleted = $collections->delete();
+
+        if ($deleted) {
+            $collections = PostCollections::all();
+            return response()->json(['check' => true, 'msg' => 'Xóa thành công', 'data' => $collections]);
+        } else {
+            return response()->json(['check' => false, 'msg' => 'Xóa thất bại']);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\Users\PermissionsController;
 use App\Http\Controllers\Collections\ProductCollection;
 use App\Http\Controllers\Categories\CategoriesController;
 use App\Http\Controllers\Posts\PostCategoryController;
+use App\Http\Controllers\Posts\PostCollectionsController;
 
 Route::prefix('admin')->middleware('auth')->group(function () {
     Route::resource('bills', BillsController::class);
@@ -28,10 +29,10 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::resource('users', UsersController::class);
     //==========================================================
     Route::resource('collections', ProductCollection::class);
-    Route::post('collections/home', [ProductCollection::class,'storeHomeCollection']);
-    Route::post('collections/home', [ProductCollection::class,'storeHomeCollection']);
-    Route::get('collections-trang-chu', [ProductCollection::class,'indexHomeCollection']);
-    Route::put('collectionsHome/{id}',[ProductCollection::class,'updateHomeCollection']);
+    Route::post('collections/home', [ProductCollection::class, 'storeHomeCollection']);
+    Route::post('collections/home', [ProductCollection::class, 'storeHomeCollection']);
+    Route::get('collections-trang-chu', [ProductCollection::class, 'indexHomeCollection']);
+    Route::put('collectionsHome/{id}', [ProductCollection::class, 'updateHomeCollection']);
     //==========================================================
     Route::resource('categories', CategoriesController::class);
     //==========================================================
@@ -42,6 +43,7 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     //==========================================================
     Route::resource('posts', PostController::class);
     Route::resource('post/categories', PostCategoryController::class);
+    Route::resource('posts-collections', PostCollectionsController::class);
     //==========================================================
     Route::resource('files', FileController::class);
     Route::resource('files', FileController::class);
@@ -50,12 +52,12 @@ Route::prefix('admin')->middleware('auth')->group(function () {
     Route::delete('folder/{id}', [FolderController::class, 'destroy']);
     //==========================================================
     Route::resource('products', ProductsController::class);
-    Route::post('/update-products/{id}', [ProductsController::class,'update']);
-    Route::post('/update-product-images/{id}', [ProductsController::class,'Update_Images']);
-    Route::post('/delete-product-image/{id}', [ProductsController::class,'Delete_Image']);
-    Route::post('/set-default-product-image/{id}', [ProductsController::class,'Set_Default']);
-    Route::get('/products-export', [ProductsController::class,'exportExample']);
-    Route::get('/products', [ProductsController::class,'index']);
+    Route::post('/update-products/{id}', [ProductsController::class, 'update']);
+    Route::post('/update-product-images/{id}', [ProductsController::class, 'Update_Images']);
+    Route::post('/delete-product-image/{id}', [ProductsController::class, 'Delete_Image']);
+    Route::post('/set-default-product-image/{id}', [ProductsController::class, 'Set_Default']);
+    Route::get('/products-export', [ProductsController::class, 'exportExample']);
+    Route::get('/products', [ProductsController::class, 'index']);
 });
 Route::get('/', [UsersController::class, 'login'])->name('login');
 Route::post('/checkLogin', [UsersController::class, 'checkLogin'])->middleware('web');
